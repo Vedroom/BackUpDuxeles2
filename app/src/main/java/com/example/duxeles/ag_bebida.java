0acdc6a2beddeda1f22f3ac5061e23641ac75744
GammaPerez <L19180927@slp.tecnm.mx>
2022-04-14 19:58:38 -0500

    AGv1.0

---
+++ app/src/main/java/com/example/duxeles/ag_bebida.java
@@ -0,0 +1,14 @@
+package com.example.duxeles;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ag_bebida extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ag_bebida);
+    }
+}
