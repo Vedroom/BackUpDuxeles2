12063c594ab5545f6d091a7cb52ac3493202ea85
GammaPerez <L19180927@slp.tecnm.mx>
2022-04-15 15:42:53 -0500

    AGv1.0

--- app/src/main/java/com/example/duxeles/ag_bebida.java
+++ app/src/main/java/com/example/duxeles/ag_bebida.java
@@ -1,17 +1,23 @@
 package com.example.duxeles;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.ContentValues;
+import android.content.Intent;
 import android.database.sqlite.SQLiteDatabase;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.Toast;
 
 public class ag_bebida extends AppCompatActivity {
 
     private EditText NomBebida, DesBebida, CantBebida, PreBebida;
+    ImageView imagen;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,6 +28,23 @@ public class ag_bebida extends AppCompatActivity {
         CantBebida = (EditText)findViewById(R.id.txtCantBebida);
         PreBebida = (EditText)findViewById(R.id.txtPreBebida);
     }
+
+    //METODO CARGAR IMG
+    public void cargarimg(){
+        Intent intent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+        intent.setType("Imagen/");
+        startActivityForResult(intent.createChooser(intent,"Seleccione la Aplicacion"),10);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(resultCode == RESULT_OK){
+            Uri path = data.getData();
+            imagen.setImageURI(path);
+        }
+    }
+
     //METODO ALTA DE BEBIDAS
    public void Register(View view){
         AdminSQLiteOpenHelper admin = new AdminSQLiteOpenHelper(ag_bebida.this);
