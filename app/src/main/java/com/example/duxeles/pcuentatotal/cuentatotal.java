71886fb43a79aafd05f73c7ecf900a1d8b8addb7
Vedroom <isra.vedroom@gmail.com>
2022-04-19 15:14:29 -0500

    Creacion del layout cuenta

--- app/src/main/java/com/example/duxeles/pcuentatotal/cuentatotal.java
+++ app/src/main/java/com/example/duxeles/pcuentatotal/cuentatotal.java
@@ -9,6 +9,7 @@ public class cuentatotal extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_cuentatotal);
     }
 }
